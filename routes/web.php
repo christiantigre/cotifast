8a029827718ead71f47924998e13215a7292c25a
Christian Tigre <andrescondo17@gmail.com>
2018-03-29 10:13:48 -0500

    Solucionado el login yregister

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,5 @@ Route::group(['prefix' => 'admin'], function () {
   Route::post('/password/reset', 'AdminAuth\ResetPasswordController@reset')->name('password.email');
   Route::get('/password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
   Route::get('/password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm');
+  Route::get('/administracion', 'Admin\\AdminController@index')->name('administracion');
 });
